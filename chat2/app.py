3ad36dddea019d02c40f5023f6ac0c93f7ac15c7
benjicurtis <benji.curtis01@gmail.com>
2026-08-02 19:05:17 -0700

    Modify message format in send_message function
    Updated message format to include sender's name separately.

--- chat2/app.py
+++ chat2/app.py
@@ -14,7 +14,7 @@ def send_message():
     name = data['name']
     message = data['msg']
     date = data['date']
-    messages.append(f"{name} {date}: {message}")
+    messages.append(f"{name}: {message}: {date}")
     return jsonify({'status': 'success', 'message': message})
 
 @app.route('/response')
